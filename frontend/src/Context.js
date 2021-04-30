317309f0ba91dde5fc71ca82eb2142b17f526e5b
emrdagkusu <emrdagkusu@gmail.com>
2021-05-01 00:53:00 +0300

    context api added for login system

---
+++ frontend/src/Context.js
@@ -0,0 +1,20 @@
+import { createContext, useState, useLayoutEffect } from "react"
+
+export const UserContext = createContext()
+
+export const UserProvider = (props) => {
+    const [login, setLogin] = useState(
+        JSON.parse(localStorage.getItem("LOGIN")) || false
+    )
+    const loginState = [login, setLogin]
+
+    useLayoutEffect(() => {
+        localStorage.setItem("LOGIN", JSON.stringify(login))
+    }, [login])
+
+    return (
+        <UserContext.Provider value={loginState}>
+            {props.children}
+        </UserContext.Provider>
+    )
+}
