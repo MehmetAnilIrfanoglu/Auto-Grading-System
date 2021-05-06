79aee504930af4fdb8b39e7ed2a3b289b0eccd66
beratbozkurt <brtbzkrt06@gmail.com>
2021-05-06 14:44:35 +0300

    Ace Editor and Judge0 Api added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./components/HomePage"
 import NotFound from "./components/NotFound"
+import CodeEditor from "./components/CodeEditor"
 
 function App() {
     return (
@@ -13,7 +14,8 @@ function App() {
                     <Route exact path="/" component={Home} />
                     {/* <Route exact path="/signin" component={Login} /> */}
                     {/* <Route exact path="/signup" component={Register} /> */}
-
+                   
+                    <Route exact path="/editor" component={CodeEditor} />
                     <Route component={NotFound} />
                 </Switch>
             </div>
